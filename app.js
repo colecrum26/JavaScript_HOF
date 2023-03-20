2f948679149586cf132ce00ea5411472cbbb5794
colecrum26 <cwcrumrine@gmail.com>
2023-03-20 13:48:13 -0500

    formatted and uncommented logs

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
-function plus(number){
-    return ((plusNumber ) => {
-       return plusNumber + number;
-    })
+function plus(number) {
+  return (plusNumber) => {
+    return plusNumber + number;
+  };
 }
 let plus15 = plus(15);
 console.log(plus15(10));
@@ -42,7 +42,7 @@ let users = [
 ];
 
 users.forEach((user) => {
-//   console.log(user.name);
+  console.log(user.name);
 });
 
 // Exercise 3 Section
@@ -51,7 +51,7 @@ console.log("EXERCISE 3:\n==========\n");
 const mapUser = users.map((user) => {
   return { name: user.name, score: user.score };
 });
-// console.log(mapUser);
+console.log(mapUser);
 
 // additional map
 // const nameAndScores = users.map((user) => {
@@ -63,12 +63,12 @@ const mapUser = users.map((user) => {
 console.log("EXERCISE 4:\n==========\n");
 
 const activeUser = users.filter((user) => {
-    return user.isActive;
-    // if (user.isActive === true){
-    //     return { name: user.name };
-    // } this creates a filter within a filter
+  return user.isActive;
+  // if (user.isActive === true){
+  //     return { name: user.name };
+  // } this creates a filter within a filter
 });
-// console.log(activeUser);
+console.log(activeUser);
 
 // additional filter
 // const isOlder = users.filter((user)=>{
@@ -79,16 +79,16 @@ const activeUser = users.filter((user) => {
 // Exercise 5 Section
 console.log("EXERCISE 5:\n==========\n");
 
-let scoreDesc = users.sort((user1, user2)=>{
-    return user2.score - user1.score;
+let scoreDesc = users.sort((user1, user2) => {
+  return user2.score - user1.score;
 });
 console.log(scoreDesc);
 
 // Exercise 6 Section
 console.log("EXERCISE 6:\n==========\n");
 
-let sumScore = users.reduce((sum, user)=>{
-    return sum + user.score;
-},0);
+let sumScore = users.reduce((sum, user) => {
+  return sum + user.score;
+}, 0);
 console.log(sumScore);
-console.log(sumScore/users.length);
+console.log(sumScore / users.length);
